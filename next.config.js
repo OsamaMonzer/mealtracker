61cde759b4437b3f1f00e10ff85504050b341332
OsamaMonzer <osamamonzer@gmail.com>
2026-06-20 01:12:14 +0800

    chore: add sqlite3 to serverComponentsExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['sqlite3', 'sqlite']
+  }
+};
 module.exports = nextConfig;
